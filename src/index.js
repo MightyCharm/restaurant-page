924567830da1fec9e7460a4215d6d274783b355b
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-10-24 17:57:38 +0200

    feat: add home site of webpage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
+/*
+import odinImage from "./restaurant.jpg";
+
+const image = document.createElement("img");
+
+image.src = odinImage;
+
+document.body.appendChild(image);
 
 console.log("Hello World");
+*/
+
+
+
+// Image by <a href="https://pixabay.com/users/julientromeur-3630051/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4289545">Julien Tromeur</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4289545">Pixabay</a>
+//Foto von Nikita Belokhonov: https://www.pexels.com/de-de/foto/stadt-restaurant-strasse-sommer-5942754/
